5118e08ad3e4c887132dc3da5338fe4eb9d1a0a8
BlueWeabo <ilia.iliev2005@gmail.com>
2023-10-10 15:09:24 +0300

    fix bugs, first try for gps

--- SmartBin.ino
+++ SmartBin.ino
@@ -7,6 +7,7 @@
 
 #include <WiFi.h>
 #include <FirebaseESP32.h>
+#include <SoftwareSerial.h>
 
 const char* ssid = "smartbin";
 const char* password = "smartbin";
@@ -19,15 +20,17 @@ const char* password = "smartbin";
 const int trigPin = 5;
 const int echoPin = 18;
 
-const int trashCanHeight = 30;
+const float trashCanHeight = 30;
 
 
 FirebaseData firebaseData;
-
+EspSoftwareSerial::UART ss;
 void setup() {
   Serial.begin(9600);
-  
-  
+  ss.begin(9600, SWSERIAL_8N1, 25,26, false);
+  if (!ss) {
+    Serial.println("Serial initialisation failed");
+  }
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -54,21 +57,23 @@ void loop() {
   long duration = pulseIn(echoPin, HIGH);
   float distance = (duration * 0.034) / 2; 
 
-  float percentage = 0.0;
+  float percentage = 0.0f;
   if (distance <= trashCanHeight) {
-    percentage = ((float)(trashCanHeight - distance) / trashCanHeight) * 100;
+    percentage = float(((trashCanHeight - distance) / trashCanHeight) * 100);
     Serial.print("Trash can is ");
     Serial.print(percentage);
     Serial.println("% full");
 
     Firebase.setFloat(firebaseData,DATABASE_PATH ,percentage);
-    if (firebaseData.dataType() == "float") {
-      Serial.print("Data saved to Firebase: ");
-      Serial.println(firebaseData.floatData());
-    } else {
-      Serial.println("Error saving data to Firebase");
-    }
-  } 
+    Serial.print("Data saved to Firebase: ");
+    Serial.println(firebaseData.floatData());
+  }
+
+  while (ss.available() > 0) {
+    byte data = ss.read();
+    Serial.write(data);
+  }
+  Serial.println("");
 
   delay(1000);
 }
