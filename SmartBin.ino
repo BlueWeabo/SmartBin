16c812d29a6faeae4c0ce971c799165877173dfd
BlueWeabo <ilia.iliev2005@gmail.com>
2023-10-16 09:12:08 +0300

    gps work

--- SmartBin.ino
+++ SmartBin.ino
@@ -3,31 +3,34 @@
  * Wifi 1.2.7
  * EspSoftwareSerial 8.1.0
  * Firebase ESP32 Client 4.3.14
+ * TinyGPSPlus 1.0.3
  */
 
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 #include <SoftwareSerial.h>
+#include <TinyGPSPlus.h>
 
 const char* ssid = "smartbin";
 const char* password = "smartbin";
-
+#define RXD2 16
+#define TXD2 17
 #define FIREBASE_HOST "https://smartbin-d94f7-default-rtdb.europe-west1.firebasedatabase.app/"
 #define FIREBASE_AUTH "96f39500cb59f6f63ca1d780e9a35b10ba1c2633"
 
-#define DATABASE_PATH "/smartbin/percentage"
+#define DATABASE_PATH "/smartbin/Bin1/percentage"
 
 const int trigPin = 5;
 const int echoPin = 18;
 
 const float trashCanHeight = 30;
 
-
+TinyGPSPlus gps;
 FirebaseData firebaseData;
 EspSoftwareSerial::UART ss;
 void setup() {
   Serial.begin(9600);
-  ss.begin(9600, SWSERIAL_8N1, 25,26, false);
+  ss.begin(9600, SWSERIAL_8N1, RXD2, TXD2, false);
   if (!ss) {
     Serial.println("Serial initialisation failed");
   }
@@ -69,11 +72,29 @@ void loop() {
     Serial.println(firebaseData.floatData());
   }
 
-  while (ss.available() > 0) {
-    byte data = ss.read();
-    Serial.write(data);
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
   }
   Serial.println("");
 
   delay(1000);
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid()){
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
 }
